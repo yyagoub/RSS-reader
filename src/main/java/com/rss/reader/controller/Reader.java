e5f00f5a20a3e14122bda0a9dd333b33fb65f668
yyagoub <imjou9@gmail.com>
2020-07-03 00:28:48 +0300

    first commit

---
+++ src/main/java/com/rss/reader/controller/Reader.java
@@ -0,0 +1,39 @@
+package com.rss.reader.controller;
+
+import java.net.URL;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rometools.rome.feed.synd.SyndEntry;
+import com.rometools.rome.feed.synd.SyndFeed;
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
+
+@RestController
+@RequestMapping("/read")
+public class Reader {
+	
+	@GetMapping
+	public String getMapping() {
+		try {
+			String url = "https://www.alarabiya.net/.mrss/ar.xml";
+
+            try (XmlReader reader = new XmlReader(new URL(url))) {
+                SyndFeed feed = new SyndFeedInput().build(reader);
+                System.out.println(feed.getTitle());
+                System.out.println("***********************************");
+                for (SyndEntry entry : feed.getEntries()) {
+                    System.out.println(entry);
+                    System.out.println("***********************************");
+                }
+                System.out.println("Done");
+            }
+		}catch(Exception e) {
+			e.getStackTrace();
+		}
+		return "Done";
+	}
+
+}
